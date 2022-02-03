1ed2558117450b32ff70e5a047e5d84f62d501a5
Aitor Ruiz <aitorruiz2m@gmail.com>
2022-02-03 17:32:23 +0100

    :bug: Category text overflow

--- components/ItemsReview/index.tsx
+++ components/ItemsReview/index.tsx
@@ -14,7 +14,7 @@ const ItemsReview: NextPage<Props> = ({ items, title }) => {
     return (
         <div className="min-h-screen">
             <div className="container md:mx-auto">
-                <h1 className="text-6xl font-bold text-center py-5">
+                <h1 className="text-5xl md:text-6xl font-bold text-center py-5 text-ellipsis">
                     {title}
                 </h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:gap-32 w-11/12 md:w-full mx-auto my-10">
