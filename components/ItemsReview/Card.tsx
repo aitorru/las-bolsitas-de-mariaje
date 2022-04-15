e8271c9b1144c6e1bda1a5fddce6448608df2a51
Aitor Ruiz <aitorruiz2m@gmail.com>
2022-04-15 20:54:49 +0200

    :lipstick: Add link to the card image

--- components/ItemsReview/Card.tsx
+++ components/ItemsReview/Card.tsx
@@ -12,25 +12,29 @@ interface Props {
 const Card: NextPage<Props> = ({ nombre, image, blur }) => {
 
     return (
-        <div className="flex flex-col p-5 border-2 mx-auto border-blue-700 rounded-xl shadow-xl shadow-blue-700/50 h-full min-h-fit min-w-full">
+        <div className="flex flex-col h-full min-w-full p-5 mx-auto border-2 border-blue-700 shadow-xl rounded-xl shadow-blue-700/50 min-h-fit">
             <div>
                 <div className='relative h-[20rem]'>
-                    <Image
-                        className="rounded-xl"
-                        alt={nombre}
-                        src={image}
-                        placeholder='blur'
-                        blurDataURL={blur}
-                        layout={'fill'} 
-                        objectFit={'contain'}
-                    />
+                    <Link passHref href={'/p/' + nombre}>
+                        <a>
+                            <Image
+                                className="rounded-xl"
+                                alt={nombre}
+                                src={image}
+                                placeholder='blur'
+                                blurDataURL={blur}
+                                layout={'fill'} 
+                                objectFit={'contain'}
+                            />
+                        </a>
+                    </Link>
                 </div>
             </div>
-            <h1 className="text-3xl w-4/5 mx-auto text-center my-3 font-bold">
+            <h1 className="w-4/5 mx-auto my-3 text-3xl font-bold text-center">
                 {nombre}
             </h1>
             <Link passHref href={'/p/' + nombre}>
-                <a className="flex flex-row justify-center py-3 w-4/5 mx-auto text-center font-semibold mt-auto bg-blue-700 shadow-2xl shadow-blue-700/10 rounded-xl text-white hover:-translate-y-1 transition-transform">
+                <a className="flex flex-row justify-center w-4/5 py-3 mx-auto mt-auto font-semibold text-center text-white bg-blue-700 shadow-2xl shadow-blue-700/10 rounded-xl hover:-translate-y-1 transition-transform">
                 Más información<Plus />
                 </a>
             </Link>
@@ -39,7 +43,7 @@ const Card: NextPage<Props> = ({ nombre, image, blur }) => {
 };
 
 const Plus = () => {
-    return <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    return <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
     </svg>;
 };
